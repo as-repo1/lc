cddf0f40378f36208d1d919a60c7a93e2967846c
as-repo1 <abhinabasarkar22@gmail.com>
2024-01-22 03:07:20 +0530

    modified:   198 	modified:   198.cpp

--- 198.cpp
+++ 198.cpp
@@ -3,22 +3,42 @@
 
 using namespace std;
 
-int main() {
-    vector<int> nums = {2, 7, 9, 3, 1};
-    int left = 0, right = 0;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // Initialize variables to keep track of the maximum amount robbed
+        int current = 0;  // Represents the maximum amount if the current house is robbed
+        int previous = 0; // Represents the maximum amount if the previous house is robbed
 
-    for (int i = 0; i < nums.size(); ++i) {
-        int temp = left;
-        left = max(right + nums[i], left);
-        right = temp;
+        // Iterate through the houses
+        for (int i = 0; i < nums.size(); ++i) {
+            // Temporarily store the current value of 'current'
+            int temp = current;
 
-        // Output for each iteration
-        cout << "Iteration " << i + 1 << ": ";
-        cout << "left = " << left << ", right = " << right << ", temp = " << temp << endl;
+            // Update 'current' to be the maximum amount if the current house is robbed or if the previous house is robbed
+            current = max(previous + nums[i], current);
+
+            // Update 'previous' to the previous value of 'current'
+            previous = temp;
+        }
+
+        // The final 'current' value represents the maximum amount that can be robbed
+        return current;
     }
+};
 
-    // Final result
-    cout << "Final Result: " << left << endl;
+int main() {
+    // Input array representing the amount of money in each house
+    vector<int> nums = {2, 7, 9, 3, 1};
+
+    // Create an instance of the Solution class
+    Solution sol;
+
+    // Call the rob function to get the result
+    int result = sol.rob(nums);
+
+    // Output the final result
+    cout << "Maximum amount that can be robbed: " << result << endl;
 
     return 0;
 }
